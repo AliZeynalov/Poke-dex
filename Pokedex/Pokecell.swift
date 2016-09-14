4ad8e747bbe86401b706b2b4dbcd5bacac71526d
Ali Zeynalov <caner.karinca@t2.com.tr>
2016-09-14 15:17:01 +0300

    second project commit

---
+++ Pokedex/Pokecell.swift
@@ -0,0 +1,32 @@
+//
+//  Pokecell.swift
+//  Pokedex
+//
+//  Created by Caner Karinca on 14/09/16.
+//  Copyright © 2016 Ali Zeynalov. All rights reserved.
+//
+
+
+import UIKit
+
+class PokeCell: UICollectionViewCell {
+    @IBOutlet weak var thumbImg: UIImageView!
+    @IBOutlet weak var nameLbl: UILabel!
+    
+    var pokemon: Pokemon!
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        
+        layer.cornerRadius = 5.0
+        
+    }
+    
+    func configureCell(pokemon: Pokemon) {
+        self.pokemon = pokemon
+        
+        nameLbl.text = self.pokemon.name.capitalizedString
+        thumbImg.image = UIImage(named: "\(self.pokemon.pokedexId)")
+        
+    }
+}
